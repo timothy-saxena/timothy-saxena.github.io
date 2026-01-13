256aa03f9c207cc1824d28d430e2ad6baf2e79bd
Timothy <timothy.saxena@gmail.com>
2026-01-13 21:49:21 +0530

    fixed my pic size, UI changes

--- docs/script.js
+++ docs/script.js
@@ -1,40 +1,3 @@
-/* const canvas = document.getElementById("matrix");
-const ctx = canvas.getContext("2d");
-
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-
-resizeCanvas();
-window.addEventListener("resize", resizeCanvas);
-
-const letters = "01ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const fontSize = 14;
-let columns = Math.floor(canvas.width / fontSize);
-let drops = Array(columns).fill(1);
-
-function draw() {
-    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = "#00ff66"; // green
-    ctx.font = fontSize + "px monospace";
-
-    for (let i = 0; i < drops.length; i++) {
-        const text = letters.charAt(Math.floor(Math.random() * letters.length));
-        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-
-        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
-            drops[i] = 0;
-        }
-
-        drops[i]++;
-    }
-}
-
-setInterval(draw, 35); // higher = slower, smoother
- */
 
 /* MATRIX BACKGROUND */
 const canvas = document.getElementById("c");
@@ -70,7 +33,7 @@ function drawMatrix() {
     }
 }
 
-setInterval(drawMatrix, 40);
+setInterval(drawMatrix, 35);
 
 /* TEXT TOGGLE */
 const text1 = document.getElementById("text1");
